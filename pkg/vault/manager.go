b27a6d0d30527579bc73a72d2bd37a91ed3ea92a
Waiariki Koia <waiariki.koia@rvu.co.uk>
2020-12-03 12:05:30 +0000

    Set error if error is nil

--- pkg/vault/manager.go
+++ pkg/vault/manager.go
@@ -155,6 +155,9 @@ func (m *DefaultManager) Save() error {
 func (m *DefaultManager) renewSecret(leaseID string) error {
 	secret, err := m.client.Sys().Renew(leaseID, int(m.lease.Seconds()))
 	if err != nil || secret == nil {
+		if err == nil {
+			err = fmt.Errorf("secret is nil")
+		}
 		log.Errorf("error renewing lease: %s", err)
 		fatalError := checkFatalError(err)
 		if fatalError != nil {
@@ -190,6 +193,9 @@ func (m *DefaultManager) renewCertificate() error {
 func renewAuth(client *api.Client, renew int) error {
 	secret, err := client.Auth().Token().RenewSelf(renew)
 	if err != nil || secret == nil {
+		if err == nil {
+			err = fmt.Errorf("secret is nil")
+		}
 		log.Errorf("error renewing token: %s", err)
 		fatalError := checkFatalError(err)
 		if fatalError != nil {
