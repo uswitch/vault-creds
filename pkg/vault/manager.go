132b109d7c04d542407f3c9dcea36221c9e7e363
Waiariki Koia <waiariki.koia@rvu.co.uk>
2020-12-03 09:00:32 +0000

    Add check for nil secret

--- pkg/vault/manager.go
+++ pkg/vault/manager.go
@@ -154,7 +154,7 @@ func (m *DefaultManager) Save() error {
 
 func (m *DefaultManager) renewSecret(leaseID string) error {
 	secret, err := m.client.Sys().Renew(leaseID, int(m.lease.Seconds()))
-	if err != nil {
+	if err != nil || secret == nil {
 		log.Errorf("error renewing lease: %s", err)
 		fatalError := checkFatalError(err)
 		if fatalError != nil {
@@ -189,7 +189,7 @@ func (m *DefaultManager) renewCertificate() error {
 
 func renewAuth(client *api.Client, renew int) error {
 	secret, err := client.Auth().Token().RenewSelf(renew)
-	if err != nil {
+	if err != nil || secret == nil {
 		log.Errorf("error renewing token: %s", err)
 		fatalError := checkFatalError(err)
 		if fatalError != nil {
